7729904a07726ae515b81224fd38f1d124b8929b
Gabriele K Silva <gabriele.s@aluno.ifsp.edu.br>
2025-04-25 20:54:53 -0300

    Initial commit

---
+++ src/main/java/br/edu/ifsp/prs/fortalezas/FortalezasApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.prs.fortalezas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortalezasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortalezasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifsp/prs/fortalezas/FortalezasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.prs.fortalezas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortalezasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
